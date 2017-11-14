ea834d5882b7eea84311313254f59638e1ca1246
Tai Doan <doantientai@gmail.com>
2017-11-13 17:55:58 -0800

    root_128 done

--- run_testing.py
+++ run_testing.py
@@ -32,7 +32,7 @@ else:
 # finally run the prediction
 if nohup:
     print "\n2. Run the prediction on GPU  with nohup"
-    os.system(run_GPU +' nohup python -u ./src/retinaNN_predict.py > ' +'./'+ +result_dir+'/'+name_experiment+'_prediction.nohup')
+    os.system(run_GPU +' nohup python -u ./src/retinaNN_predict.py > ' + './' +result_dir +'/'+name_experiment+'_prediction.nohup')
 else:
     print "\n2. Run the prediction on GPU (no nohup)"
     os.system(run_GPU +' python ./src/retinaNN_predict.py')
