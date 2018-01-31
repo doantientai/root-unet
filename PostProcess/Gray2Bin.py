6e5b7326ddb8e41a3c6ed4341f08adb06f0b3f6a
Tai Doan <doantientai@gmail.com>
2018-01-30 20:29:33 -0800

    update

--- PostProcess/Gray2Bin.py
+++ PostProcess/Gray2Bin.py
@@ -1,55 +1,66 @@
+# input_path = '/home/doantientai/Projects/DeepRoot/root-unet/Experiments/root_64_rgb/TestResults/root_64_rgb_1_predict.png'
 
-# coding: utf-8
+input_path = '/home/doantientai/Projects/DeepRoot/root-unet/Experiments/root_128_rgb_crop256_flrt_500e/TestResults/root_128_rgb_crop256_flrt_500e_0_predict.png'
 
-# In[43]:
-
-
-input_path = '/home/doantientai/Projects/DeepRoot/root-unet/Experiments/root_64_rgb/TestResults/root_64_rgb_1_predict.png'
+gt_path = '/home/doantientai/Projects/DeepRoot/root-unet/Experiments/root_128_rgb_crop256_flrt_500e/TestResults/root_128_rgb_crop256_flrt_500e_0_gtruth.png'
 
 from PIL import Image
 from matplotlib.pyplot import imshow, imsave
 import numpy as np
-get_ipython().magic('matplotlib inline')
+# %matplotlib inline
 import scipy.ndimage
-import cv3
 
 
-# In[37]:
+# import os
+# os.sys.path
 
+# import sys
+# sys.path.append('/home/doantientai/anaconda3/envs/keras1.1/lib/python2.7/site-packages')
+import cv2
 
-im = Image.open(input_path)
+from skimage import morphology
+
+# input: grayscale predicted image
+# convert to binary, invert
+# remove small objects
+
+# im = Image.open(input_path)
+im = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
 # imshow(np.asarray(im),cmap="gray")
 # imsave(im, "img_gray.png")
-im.save("im_gray.png")
+cv2.imwrite("im_pred.png", im)
+
+im_gt = cv2.imread(gt_path, cv2.IMREAD_GRAYSCALE)
+cv2.imwrite("im_gt.png", im_gt)
+
+im_bin = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,31,2)
+# cv2.imwrite("im_bin.png", im_bin)
+
+im_inv = cv2.bitwise_not(im_bin)
+# cv2.imwrite("im_inv.png", im_inv)
+
+kernel_erode = np.ones((2,2),np.uint8)
+im_ero = cv2.erode(im_inv,kernel_erode,iterations = 1)
+# cv2.imwrite('im_ero.png', im_ero)
+
+kernel_close = np.ones((5,5),np.uint8)
+im_close = closing = cv2.morphologyEx(im_ero, cv2.MORPH_CLOSE, kernel_close)
+# cv2.imwrite('im_close.png', im_close)
+
+im_dn = morphology.remove_small_objects(im_close.astype('bool'),64)
+im_dn = np.array(im_dn, dtype=np.uint8)
+
+kernel_close = np.ones((5,5),np.uint8)
+im_dn = closing = cv2.morphologyEx(im_dn, cv2.MORPH_CLOSE, kernel_close)
+# cv2.imwrite('im_close.png', im_close)
 
 
-# In[38]:
+im_dn*=255
 
+# cv2.imwrite('im_dn.png', im_dn)
 
-im_array = np.asarray(im)
-print(im_array.shape)
-# THRESHOLD_VALUE = 225
+im_dn_inv = cv2.bitwise_not(im_dn)
+cv2.imwrite("im_dn_inv.png", im_dn_inv)
 
-# im_array_bin = (im_array > THRESHOLD_VALUE) * 255.0
-
-# scipy.ndimage.morphology.binary_fill_holes(im_array_bin)
-
-# im_bin = Image.fromarray(im_array_bin).convert('L')
-# # im_bin
-# # im_array_bin.shape
-
-
-th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,            cv2.THRESH_BINARY,11,2)
-
-
-im_bin.save("img_bin.png")
-# imshow(im_bin)
-# im_bin
-im_bin
-
-
-# In[32]:
-
-
-10%2
-
+# print np.amax(im_dn.astype('uint8'))
+# imshow(im_dn,cmap='binary')
